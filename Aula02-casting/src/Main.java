35639be78040307225f53ea743fb748b2dbaf4cc
Cleiton <tirandoonda2014@gmail.com>
2022-02-28 11:39:40 -0300

    Finalizado exercicio 2

--- Aula02-casting/src/Main.java
+++ Aula02-casting/src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double a;
+		int b;
+		
+		b = a; 
+		
+		b = (int) a - fazer com que b receba o valor de a como inteiro independente do tipo de variavel
+		
+		
 
 	}
 
