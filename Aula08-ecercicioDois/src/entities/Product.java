94ef0ce0011cf4fd20daab2bcb5d0c53b9ef0522
Cleiton <tirandoonda2014@gmail.com>
2022-03-05 20:01:43 -0300

    Finalizado classe Product do exercicio

--- Aula08-ecercicioDois/src/entities/Product.java
+++ Aula08-ecercicioDois/src/entities/Product.java
@@ -1,5 +1,21 @@
 package entities;
 
 public class Product {
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock () {
+		return price * quantity;
+	}
+	
+	public void addProducts (int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void remveProducts (int quantity) {
+		this.quantity -= quantity;
+	}
 
 }
