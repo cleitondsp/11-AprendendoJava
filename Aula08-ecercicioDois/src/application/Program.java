156f332d8cb4dcc9fdadd843d054acc2958435f6
Cleiton <tirandoonda2014@gmail.com>
2022-03-06 19:33:32 -0300

    Testando a entrada dos dados

--- Aula08-ecercicioDois/src/application/Program.java
+++ Aula08-ecercicioDois/src/application/Program.java
@@ -1,10 +1,43 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product(); // importando a classe Product pra ser usada
+		
+		System.out.println("Enter the product data.");
+		
+		System.out.print("Name: ");		
+		product.name = sc.nextLine();
+		
+		System.out.print("Price: ");		
+		product.price = sc.nextDouble();
+		
+		System.out.print("Quantity in stock: ");		
+		product.quantity = sc.nextInt();
+		
+		
+		System.out.println(product.name);
+		System.out.println(product.price);
+		System.out.println(product.quantity);
+		
+		
+		
+		
+		
+		
+				
+				
+		sc.close();
 	}
 
 }
