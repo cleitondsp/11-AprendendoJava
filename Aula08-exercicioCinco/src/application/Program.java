060cd579851debe79600a9f22e36071e5c1f2aca
Cleiton <tirandoonda2014@gmail.com>
2022-03-10 20:07:54 -0300

    Finalizado exercicio 5

--- Aula08-exercicioCinco/src/application/Program.java
+++ Aula08-exercicioCinco/src/application/Program.java
@@ -1,10 +1,47 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Media;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Media media = new Media();
+		
+		String name;
+		double falta;
+		
+		System.out.print("Digite o nome e as três notas do aluno: ");
+		name = sc.nextLine();
+		media.nota1 = sc.nextDouble();
+		media.nota2 = sc.nextDouble();
+		media.nota3 = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println(name);
+		System.out.println("FINAL GRADE = " + media.notaFinal());
+		System.out.println(media.status());
+		
+		if (media.notaFinal() < 60) {
+			falta = 60 - media.notaFinal();
+			System.out.println("MISSING " 
+								+ String.format("%.2f", falta) // faltou colocar essa formatação
+								+ " POINTS");
+		}
+		
 
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
--- Aula08-exercicioCinco/src/entities/Media.java
+++ Aula08-exercicioCinco/src/entities/Media.java
@@ -1,5 +1,23 @@
 package entities;
 
 public class Media {
+	
+	public double nota1;
+	public double nota2;
+	public double nota3;
+	
+	public double notaFinal() {
+		
+		return nota1 + nota2 + nota3;		
+	}
+	
+	public String status() {
+		
+		if (notaFinal() > 60) {
+			return "PASS";
+		} else {
+			return "FAILED";
+		}
+	}
 
 }
