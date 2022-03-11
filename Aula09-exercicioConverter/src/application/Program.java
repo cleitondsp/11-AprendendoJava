50f2827398be75c5cb1b2da439787b07e53503d1
Cleiton <tirandoonda2014@gmail.com>
2022-03-11 20:35:57 -0300

    Finalizado exercicio de membros Static

--- Aula09-exercicioConverter/src/application/Program.java
+++ Aula09-exercicioConverter/src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.currentConverter;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("What is the dollar price? ");
+		currentConverter.price = sc.nextDouble();
+		System.out.print("How many dollars will be bought? ");
+		currentConverter.amount = sc.nextDouble();
+		System.out.print("Amount to be paid in reais = " 
+				        + String.format("%.2f", currentConverter.conversor()));
+		
+		
+		
+		sc.close();
 	}
 
 }
--- Aula09-exercicioConverter/src/entities/currentConverter.java
+++ Aula09-exercicioConverter/src/entities/currentConverter.java
@@ -1,5 +1,14 @@
 package entities;
 
 public class currentConverter {
+	
+	public static double price;
+	public static double amount;
+	
+	public static double conversor() {
+		
+		return price * amount * 1.06;
+		
+	}
 
 }
