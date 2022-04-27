db71d2aba7f50c30e55edf215ac30621efeb0630
cleitondsp <tirandoonda2014@gmail.com>
2022-04-26 21:17:54 -0300

    Codigo completo do desaio 5

--- HackerHank-JavaOutputFormatting/src/application/Program.java
+++ HackerHank-JavaOutputFormatting/src/application/Program.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("================================");
 		
-		Scanner sc=new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+
+			String s1 = sc.next();
+			int x = sc.nextInt();
+			String espaco = " ";
+
+			int carac = s1.length() + 3;
+			int qtdeEspacos = 18 - carac;
+
+			for (int j = 1; j < qtdeEspacos; j++) {
+				espaco += " ";
+			}
+
+			System.out.println(s1 + espaco + String.format("%03d", x));
+		}
 		
-		String s1=sc.next();
-        int x=sc.nextInt();
-        String espaco = " ";
-        
-        int carac = s1.length() + 3;     
-        int qtdeEspacos = 18 - carac;    
-        
-        for (int j = 1; j < qtdeEspacos; j++) {
-        	espaco += " ";
-        }
-        
-        System.out.println(s1 + espaco + String.format("%03d", x));
-		
+		System.out.println("================================");
+
 		sc.close();
 	}
 
